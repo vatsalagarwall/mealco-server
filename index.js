8a067cf632807f9d5e62ba9677e956aa178e8a64
Vatsal Agarwal <bindalvatsaljpr@gmail.com>
2024-02-28 19:03:27 +0530

    commit

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.listen(5050, () => {
 
 app.use(
     cors({
-        origin: ["http://localhost:3000"],
+        origin: ["https://mealco-client.vercel.app"],
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     })
